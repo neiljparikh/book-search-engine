53765c71c262851785cd625233240b4807c9773d
neiljparikh <147933251+neiljparikh@users.noreply.github.com>
2024-02-09 15:53:09 -0500

    Built out typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,24 +1,32 @@
 const typeDefs = `
-  type Tech {
-    _id: ID!
-    name: String!
+  type Book {
+    authors: [String]!
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+
   }
 
-  type Matchup {
-    _id: ID!
-    tech1: String!
-    tech2: String!
-    tech1_votes: Int
-    tech2_votes: Int
+  type User {
+    username: String!
+    email: String!
+    password: String!
+    savedBooks: [String]
+  }
+
+  type Auth {
+    token: ID!
+    user: User
   }
 
   type Query {
-    tech: [Tech]
-    matchups(_id: String): [Matchup]
+    User: User
   }
 
   type Mutation {
-    createMatchup(tech1: String!, tech2: String!): Matchup
+    createUser(username: String!, email: String!, password: String!): 
     createVote(_id: String!, techNum: Int!): Matchup
   }
 `;
